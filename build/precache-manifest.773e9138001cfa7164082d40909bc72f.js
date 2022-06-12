c882b8a924f7af552a05bec0bfb1ad7de04b8e90
Amy Huang <amyxjhuang@berkeley.edu>
2022-06-11 19:01:24 -0700

    dlskajflkdsaj

---
+++ build/precache-manifest.773e9138001cfa7164082d40909bc72f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61cbe4e5f326acdbc93293d3b97bf0e4",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "2d2118624821420ddf33",
+    "url": "/weather/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "020ab6c59b5d2f988767",
+    "url": "/weather/static/css/main.0218e8a7.chunk.css"
+  },
+  {
+    "revision": "2d2118624821420ddf33",
+    "url": "/weather/static/js/2.1cdd43cc.chunk.js"
+  },
+  {
+    "revision": "020ab6c59b5d2f988767",
+    "url": "/weather/static/js/main.f4da7d47.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "12812f5d9babdd4e6f177ecf33f58f37",
+    "url": "/weather/static/media/logo.12812f5d.svg"
+  }
+]);
